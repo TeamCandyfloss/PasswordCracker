1bcc9512608312c484de9a8d6a6dd8d178de21ff
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-10-10 13:12:24 +0200

    add metode og return metode

---
+++ Client/PasswordCrackerClient/ResultManager.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerClient
+{
+    static class ResultManager
+    {
+        private static Dictionary<string, string> result = new Dictionary<string, string>();
+        private static object resultLock = new  object();
+        
+
+        public static void AddResult(string partialResult)
+        {
+            lock (resultLock)
+            {
+                string[] splitResult = partialResult.Split(':');
+                result.Add(splitResult[0], splitResult[1]);
+            }
+        }
+
+        public static Dictionary<string, string> ReturnResult()
+        {
+            return result;
+        }
+
+
+        
+
+
+    }
+}
