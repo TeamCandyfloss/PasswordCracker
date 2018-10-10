1a98d9b50b3b737cd5382a2e7d016999be84e4ed
Nikolaj Dyring Jensen <Nikodyring@gmail.com>
2018-10-10 10:14:11 +0200

    Model Classes

---
+++ Client/PasswordCrackerClient/model/UserInfoConverted.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerClient.model
+{
+    [Serializable]
+    public class UserInfoConverted
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public UserInfoConverted(string username, string password)
+        {
+            if(username == null)
+            {
+                throw new ArgumentNullException("username");
+            }
+            if(password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            Username = username;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return Username + ":" + Password;
+        }
+    }
+}
