1d924f90587f2039134d6635ade304ea8781ffcd
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-10-10 11:27:37 +0200

    Lavet SendData metode i Client

---
+++ Client/PasswordCrackerClient/SendDataToMaster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerClient
+{
+    class SendDataToMaster
+    {
+        private Stream _stream;
+
+        public SendDataToMaster(Stream stream)
+        {
+            _stream = stream;
+        }
+
+
+        
+        public void DataSendToMaster(Dictionary<string, string> dataDictionary)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(_stream,dataDictionary);
+        }
+    }
+}
